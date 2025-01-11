b0bb06c7a8bb8a094f32dc8c4fb7a903a36270b1
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-01-11 14:53:53 +0500

    updated

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+app.use(express.json())
+
+app.use('/test', (req, res) => {
+  res.send("test 1 completed")
+})
+
+app.use('/test2', (req, res) => {
+  res.send("test 1 completed")
+})
+
+app.listen(1956, () => {
+  console.log("server  on va  in too 1956 port");
+});
